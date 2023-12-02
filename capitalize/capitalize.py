b0116534496d0d0952f33d1dc3e719053c31e2d3
pavelgrebenkov <pavellearnnow@gmail.com>
2023-12-02 20:36:46 +0200

    all directories and files of capitalize-project

---
+++ capitalize/capitalize.py
@@ -0,0 +1,9 @@
+# Capilalize first letter of input string
+
+
+def capitalize(text):
+    if text == '':
+        return ''
+    first_char = text[0].upper()
+    rest_substring = text[1:]
+    return f'{first_char}{rest_substring}'
