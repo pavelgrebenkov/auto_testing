936c3000148026b1fdcdc1ff7bdae52a9b2c97fb
pavelgrebenkov <pavellearnnow@gmail.com>
2023-12-06 18:48:42 +0200

    modified tests in test_capitalize.py

--- capitalize/capitalize.py
+++ capitalize/capitalize.py
@@ -2,8 +2,8 @@
 
 
 def capitalize(text):
-    if text == '':
-        return ''
-    first_char = text[0].upper()
-    rest_substring = text[1:]
-    return f'{first_char}{rest_substring}'
+   if text == '':
+       return ''
+   first_char = text[0].upper()
+   rest_substring = text[1:]
+   return f'{first_char}{rest_substring}'
--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,17 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+# PHASE-1 (tests writteen via 'if' and 'exception')
+# if capitalize('hello') != 'Hello':
+    # raise Exception('Функция работает неверно!')
+
+# if capitalize('') != '':
+    # raise Exception('Функция работает неверно!')
+
+
+# PHASE-2 (tests written via 'assert')
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
+
 
 print('Все тесты пройдены!')
